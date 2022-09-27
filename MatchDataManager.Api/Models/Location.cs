f894f187af25a45a11cb11c4cb375154558f7d62
Kamel BENDIMERAD <kamel.bendimerad@gmail.com>
2022-09-27 16:13:22 +0200

    String validation of Name and city

--- MatchDataManager.Api/Models/Location.cs
+++ MatchDataManager.Api/Models/Location.cs
@@ -1,8 +1,14 @@
-﻿namespace MatchDataManager.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchDataManager.Api.Models;
 
 public class Location : Entity
 {
+    
+    [Required]
+    [StringLength(255, ErrorMessage = "The Name cannot exceed 255 characters. ")]
     public string Name { get; set; }
-
+    [Required]
+    [StringLength(55, ErrorMessage = "The City's name cannot exceed 55 characters. ")]
     public string City { get; set; }
 }
--- MatchDataManager.Api/Models/Team.cs
+++ MatchDataManager.Api/Models/Team.cs
@@ -1,8 +1,13 @@
-﻿namespace MatchDataManager.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MatchDataManager.Api.Models;
 
 public class Team : Entity
 {
+    [Required]
+    [StringLength(255, ErrorMessage = "The Name cannot exceed 255 characters. ")]
     public string Name { get; set; }
+    
 
     public string CoachName { get; set; }
 }
